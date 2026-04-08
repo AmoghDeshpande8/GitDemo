a0be116a9cd01f5dbeea4f4f0ff35833a2dbfd13
Amogh Deshpande <amogh.deshpande21@vit.edu>
2026-04-08 21:15:19 +0530

    first commit

---
+++ src/main/java/com/itp/controller/MessageController.java
@@ -0,0 +1,15 @@
+package com.itp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo-api")
+public class MessageController {
+
+    @GetMapping("/msg")
+    public String getMessage() {
+        return "Hello from Main Branch";
+    }
+}
